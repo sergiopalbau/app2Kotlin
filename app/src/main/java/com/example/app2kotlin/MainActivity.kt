661b6f9601978424799f973372d2928c779d4eb1
sergio <s.palomo.b@gmail.com>
2022-06-02 22:22:40 +0200

    segunda activity

--- app/src/main/java/com/example/app2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app2kotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.app2kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button);
+        btn.setOnClickListener {
+            val intent = Intent(this,Activity2::class.java)
+            startActivity(intent)
+        }
     }
 }
